b021787754dd1c25473103fe4cd783c8816cde39
Howard Chen <hchen7913@gmail.com>
2025-12-04 22:13:04 -0600

    Begin working on writing

---
+++ src/Webb/Writer/ForEach.purs
@@ -0,0 +1,96 @@
+module Webb.Writer.ForEach where
+
+import Prelude
+
+import Control.Monad.State (class MonadState, State, execState)
+import Data.Array as A
+import Data.Maybe (Maybe(..), isJust)
+import Data.TraversableWithIndex (class TraversableWithIndex, forWithIndex)
+import Unsafe.Coerce (unsafeCoerce)
+import Webb.State.Prelude (class ReferM, mmodify_)
+
+
+class Length a where
+  length :: a -> Int
+  
+instance Length (Array a) where
+  length = A.length
+
+type St = 
+  { each :: Maybe Void
+  , between :: Maybe Void
+  , after :: Maybe Void
+  , before :: Maybe Void
+  }
+
+newtype ForEach :: forall k1 k2 k3. k1 -> k2 -> k3 -> Type -> Type
+newtype ForEach m a b x = FE (State St x)
+
+derive newtype instance Functor (ForEach m a b)
+derive newtype instance Apply (ForEach m a b)
+derive newtype instance Applicative (ForEach m a b)
+derive newtype instance Bind (ForEach m a b)
+derive newtype instance Monad (ForEach m a b)
+derive newtype instance MonadState St (ForEach m a b)
+derive newtype instance ReferM St (ForEach m a b)
+
+forEach :: forall m t a b. 
+  Monad m => TraversableWithIndex Int t => Length (t a) => Monoid (t b) =>
+  t a -> ForEach m a b Unit -> m (t b)
+forEach t (FE prog) = do 
+  let fs = execState prog empty 
+  results <- forWithIndex t \i a -> do
+    let before' = unsafeCoerce $ prep fs.before :: a -> m Unit
+        each' = unsafeCoerce $ prep fs.each :: a -> m b
+        between' = unsafeCoerce $ prep fs.between :: a -> m Unit
+        after' = unsafeCoerce $ prep fs.after :: a -> m Unit
+    before' a
+    b <- each' a
+    when (isBetween t i) do between' a
+    after' a
+    pure b
+    
+  if isJust fs.each then do
+    pure results
+  else do
+    pure mempty
+  
+  where 
+  prep :: Maybe Void -> (a -> m Unit)
+  prep mf = case mf of
+    Nothing -> (\_ -> pure unit)
+    Just f -> unsafeCoerce f
+
+  n = Nothing
+  empty = { each: n, between: n, after: n, before: n}
+  isBetween coll i = let 
+    len = length coll
+    in if len <= 1 then
+      -- 0 or 1 element has no between.
+      false
+    else if i < len - 1 then
+      -- if len is 4, then while we aren't on the last index, we are between.
+      true
+    else 
+      false
+      
+forEach_ :: forall m t a b. 
+  Monad m => TraversableWithIndex Int t => Length (t a) => Monoid (t b) =>
+  t a -> ForEach m a b Unit -> m Unit
+forEach_ t prog = void $ forEach t prog
+
+before :: forall a b m. Monad m => (a -> m Unit) -> ForEach m a b Unit
+before f = do
+  mmodify_ $ _ { before = unsafeCoerce f }
+
+each :: forall a b m. Monad m => (a -> m b) -> ForEach m a b Unit
+each f = do
+  mmodify_ $ _ { each = unsafeCoerce f }
+
+inBetween :: forall a b m. Monad m => (a -> m Unit) -> ForEach m a b Unit
+inBetween f = do
+  mmodify_ $ _ { between = unsafeCoerce f }
+
+after :: forall a b m. Monad m => (a -> m Unit) -> ForEach m a b Unit
+after f = do
+  mmodify_ $ _ { after = unsafeCoerce f }
---
+++ src/Webb/Writer/Internal.purs
@@ -0,0 +1,126 @@
+module Webb.Writer.Internal where
+
+import Prelude
+import Webb.Writer.ForEach
+
+import Control.Monad.State (StateT)
+import Data.Map (Map)
+import Data.Map as M
+import Data.Traversable (class Traversable)
+import Data.Tuple (Tuple(..))
+
+
+{- The writer monad enables us to write to the monoidal source. It has to maintain the indent level in such a way that changes after a newline. Thus, we write to a thing, and utilize the indent level. Newlines ... are special. To prevent newlines from causing a problem, though, perhaps we have to count the newlines and reset the indent level when needed.
+-}
+
+
+type Position = 
+  { indentSpaces :: Int
+  , line :: Int
+  , column :: Int
+  , output :: Array String
+  }
+
+type WriterM' m a = StateT Position m a
+type WriterM m = WriterM' m Unit
+
+-- TODO -- write a token, adding a space if needed. We don't need it
+-- if we're at the start of the (indented) line.
+token :: forall m. Monad m => String -> WriterM m
+token t = pure unit
+
+-- Add an indent to the current indent, requiring all subsequent writes to
+-- be at that indent or later. Restore the current indent once the inner program
+-- is done running.
+indent :: forall m. Monad m => Int -> WriterM m -> WriterM m
+indent n prog = do pure unit
+
+-- TODO -- Put a newline. This should reset things so that we know to
+-- be back at the indent, the next time we write.
+putNewline :: forall m . Monad m => WriterM m 
+putNewline = do pure unit
+
+equals :: forall m. Monad m => WriterM m
+equals = token "="
+
+putAlias :: forall m . Monad m => String -> WriterM m 
+putAlias name = do 
+  token "type" *> token name *> equals
+
+-- { k1 : v1
+-- , k2 : v2
+-- }
+putRecord :: forall m . Monad m => Map String String -> WriterM m 
+putRecord = putRecord' ":"
+
+-- { k1 :: v1
+-- , k2 :: v2
+-- }
+putRecordType :: forall m . Monad m => Map String String -> WriterM m 
+putRecordType = putRecord' "::"
+
+-- s { k1 = v1
+-- , k2 = v2
+-- }
+putRecordUpdate :: forall m . Monad m => String -> Map String String -> WriterM m 
+putRecordUpdate name map = do 
+  token name
+  putRecord' "=" map
+
+-- Generator for writing the data.
+putRecord' :: forall m. Monad m => String -> Map String String -> WriterM m 
+putRecord' sep map = do
+  token "{"
+  putNewline
+
+  let arr = M.toUnfoldable map :: Array _
+  forEach_ arr do
+    each \(Tuple key typ) -> do
+      token key *> token sep *> token typ
+    inBetween \_ -> do
+      putNewline *> token ","
+    
+  putNewline
+  token "}"
+  
+-- Write the entire signature.
+putSignature :: forall m. Monad m => 
+  String -> Array String -> Array String -> Array String -> WriterM m
+putSignature name types classes args = do
+  token name *> token "::" *> token "forall"
+  forEach_ types do each token
+
+  token "."
+  forEach_ classes do
+    each token 
+    after \_ -> token "=>"
+
+  forEach_ args do
+    each token
+    inBetween \_ -> token "->"
+  
+-- Put the initial part of the function body, but not all the body.
+bodyEquals :: forall m. Monad m => 
+  String -> Array String -> WriterM m
+bodyEquals name args = do
+  token name 
+  forEach_ args  do each token
+  equals
+
+-- Puts the initial part of the newtype.
+newtypeEquals :: forall m. Monad m => 
+  String -> String -> WriterM m
+newtypeEquals name ctor = do
+  token "newtype" *> token name *> equals *> token ctor
+
+dataEquals :: forall m. Monad m => WriterM m
+dataEquals = do token "data" *> equals
+  
+-- Write a type, whether that's "Either a b" or "a" or "Boolean"
+putType :: forall m. Monad m => String -> Array String -> WriterM m
+putType name args = do
+  token name 
+  forEach_ args do each token
+  
+putDo :: forall m. Monad m => WriterM m
+putDo = do token "do"
