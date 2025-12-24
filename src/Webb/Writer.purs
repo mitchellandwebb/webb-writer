892718a37d446741b5298da5b9189427ca7cb36b
Howard Chen <hchen7913@gmail.com>
2025-12-24 14:13:09 -0600

    Remove the 'file' and 'directory' operations that don't belong in this package

--- src/Webb/Writer.purs
+++ src/Webb/Writer.purs
@@ -1,21 +1,11 @@
 module Webb.Writer 
 ( module P
-, flushToFile
 )
 where
 
 import Prelude
 
 import Effect.Aff.Class (class MonadAff)
-import Webb.Writer.File (File)
-import Webb.Writer.File (File, assertProjectRoot, close, getFile, isOpen, openAndTruncate, resetDir) as P
-import Webb.Writer.File as F
 import Webb.Writer.Internal (WriterM, clearOutput, getOutput)
 import Webb.Writer.Internal (Position, St, WriterM, WriterM', bodyEquals, clearOutput, dataEquals, default, defaultPos, equals, evalWriterM, execWriterM, getOutput, getPosition, moduleWhere, multiLine, newline, newtypeEquals, putAdo, putDo, putImport, putRecord, putRecord', putRecordType, putRecordUpdate, putSignature, putType, runWriterM, string, token, typeEquals, withIndent) as P
 
-
-flushToFile :: forall m. MonadAff m => File -> WriterM m
-flushToFile file = do
-  output <- getOutput
-  clearOutput
-  F.write file output
